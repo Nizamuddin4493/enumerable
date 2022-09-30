c1877dc6687512d8e0806672a3f2668fa29803e1
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-30 20:36:23 +0430

    connecting the enumerable to list file

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,7 @@
+require_relative 'enumerable'
+
 class MyList
+  include Enumerable
   def initialize(*list)
     @list = list
   end
@@ -8,4 +11,16 @@ class MyList
   end
 end
 
-# p MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
+
+p(list.all? { |element| element > 5 })
+
+p(list.all? { |element| element < 5 })
+
+p(list.any? { |element| element > 2 })
+
+p(list.any? { |element| element < 1 })
+
+p(list.filter? { |element| element > 2 })
+
+p(list.filter? { |element| element < 5 })
